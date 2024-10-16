56353ed18f385e247552a715e37e7c5931799929
Balagangadhar-Dev <erootbala@gmail.com>
2024-10-16 12:28:51 +0530

    added models

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    GENRE_CHOICES = [
+        ('Action', 'Action'),
+        ('Drama', 'Drama'),
+        ('Comedy', 'Comedy'),
+        ('Horror', 'Horror'),
+    ]
+
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    genre = models.CharField(max_length=50, choices=GENRE_CHOICES)
+    release_date = models.DateField()
+    poster = models.ImageField(upload_to='posters/', blank=True, null=True)
+
+
